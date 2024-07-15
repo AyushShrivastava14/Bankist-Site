589480a85839bf52dd9f5e51bb1b6fa9862ec2ca
Ayush Shrivastava <ayush.shrivastava.023@gmail.com>
2024-07-15 17:39:28 +0530

    Smooth Section Reveal

--- script.js
+++ script.js
@@ -174,3 +174,24 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 headerObserver.observe(header);
 
 
+const sections = document.querySelectorAll('.section');
+
+const revealSection = function(entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if(!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+}
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15
+});
+
+sections.forEach(section => {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
